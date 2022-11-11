83403f31207abadb79e7528acb0120c26fa1b15b
Brandon Michaud <brandondmichaud@ou.edu>
2022-11-10 18:21:05 -0600

    Rough draft

--- project2.c
+++ project2.c
@@ -5,12 +5,245 @@
 struct mem {
 	int size;
 	int start;
-	char *name;
+	char name[16];
 	struct mem *next;
 };
 typedef struct mem mem;
 
-void release(mem *memory, char name[16]) {
+mem* bestFit(mem **head, char name[16], int size, int totalMem) {
+	mem *currNode = *head;
+	if (currNode == NULL) {
+		if (size <= totalMem) {
+			mem *new = (mem*) malloc(sizeof(mem));
+			new->size = size;
+			new->start = 0;
+			strncpy(new->name, name, 16);
+			new->next = NULL;
+			*head = new;
+			printf("ALLOCATED %s %d\n", name, 0);
+			return new;
+		}
+		else {
+			printf("FAIL REQUEST %s %d\n", name, size);
+			return NULL;
+		}
+	}
+	else {
+		int bestSize = totalMem + 1;
+		int bestLocation = -1;
+		int curr = 0;
+		while (currNode != NULL) {
+			if (currNode->start - curr < bestSize && currNode->start - curr >= size) {
+				bestSize = currNode->start - curr;
+				bestLocation = curr;
+			}
+			curr = currNode->start + currNode->size;
+			currNode = currNode->next;
+		}
+		if (totalMem - curr < bestSize && totalMem - curr >= size) {
+			bestSize = totalMem - curr;
+			bestLocation = curr;
+		}
+		if (bestLocation != -1) {
+			mem *new = (mem*) malloc(sizeof(mem));
+			new->start = bestLocation;
+			new->size = size;
+			strncpy(new->name, name, 16);
+			mem *start = *head;
+			if (start->start > bestLocation) {
+				new->next = *head;
+				*head = new;
+			}
+			else {
+				currNode = *head;
+				while (currNode != NULL && currNode->start + currNode->size < new->start) {
+					currNode = currNode->next;
+				}
+				new->next = currNode->next;
+				currNode->next = new;
+			}
+			printf("ALLOCATED %s %d\n", name, new->start);
+			return new;
+		}
+		else {
+			printf("FAIL REQUEST %s %d\n", name, size);
+			return NULL;
+		}
+	}
+}
+
+mem* firstFit(mem **head, char name[16], int size, int totalMem) {
+	mem *currNode = *head;
+	if (currNode == NULL) {
+		if (size <= totalMem) {
+			mem *new = (mem*) malloc(sizeof(mem));
+			new->size = size;
+			new->start = 0;
+			strncpy(new->name, name, 16);
+			new->next = NULL;
+			*head = new;
+			printf("ALLOCATED %s %d\n", name, 0);
+			return new;
+		}
+		else {
+			printf("FAIL REQUEST %s %d\n", name, size);
+			return NULL;
+		}
+	}
+	else {
+		int curr = 0;
+		mem *last = NULL;
+		while (currNode != NULL) {
+			if (currNode->start - curr >= size) {
+				mem *new = (mem*) malloc(sizeof(mem));
+				new->start = curr;
+				new->size = size;
+				strncpy(new->name, name, 16);
+				if (last != NULL) {
+					new->next = last->next;
+					last->next = new;
+				}
+				else {
+					new->next = *head;
+					*head = new;
+				}
+				printf("ALLOCATED %s %d\n", name, new->start);
+				return new;
+			}
+			curr = currNode->start + currNode->size;
+			last = currNode;
+			currNode = currNode->next;
+		}
+		if (totalMem - curr >= size) {
+			mem *new = (mem*) malloc(sizeof(mem));
+			new->start = curr;
+			new->size = size;
+			strncpy(new->name, name, 16);
+			new->next = last->next;
+			last->next = new;
+			printf("ALLOCATED %s %d\n", name, new->start);
+			return new;
+		}
+		else {
+			printf("FAIL REQUEST %s %d\n", name, size);
+			return NULL;
+		}
+	}
+}
+
+mem* nextFit(mem **head, char name[16], int size, int totalMem, mem *next) {
+	mem *currNode = *head;
+	if (currNode == NULL) {
+		if (size <= totalMem) {
+			mem *new = (mem*) malloc(sizeof(mem));
+			new->size = size;
+			new->start = 0;
+			strncpy(new->name, name, 16);
+			new->next = NULL;
+			*head = new;
+			printf("ALLOCATED %s %d\n", name, 0);
+			return new;
+		}
+		else {
+			printf("FAIL REQUEST %s %d\n", name, size);
+			return NULL;
+		}
+	}
+	else if (next == NULL) {
+		return firstFit(head, name, size, totalMem);
+	}
+	else {
+		currNode = next->next;
+		int curr = next->start + next->size;
+		mem *last = next;
+		while (currNode != NULL) {
+			if (currNode->start - curr >= size) {
+				mem *new = (mem*) malloc(sizeof(mem));
+				new->start = curr;
+				new->size = size;
+				strncpy(new->name, name, 16);
+				if (last != NULL) {
+					new->next = last->next;
+					last->next = new;
+				}
+				else {
+					new->next = *head;
+					*head = new;
+				}
+				printf("ALLOCATED %s %d\n", name, new->start);
+				return new;
+			}
+			curr = currNode->start + currNode->size;
+			last = currNode;
+			currNode = currNode->next;
+		}
+		return firstFit(head, name, size, totalMem);
+	}
+}
+
+mem* worstFit(mem **head, char name[16], int size, int totalMem) {
+	mem *currNode = *head;
+	if (currNode == NULL) {
+		if (size <= totalMem) {
+			mem *new = (mem*) malloc(sizeof(mem));
+			new->size = size;
+			new->start = 0;
+			strncpy(new->name, name, 16);
+			new->next = NULL;
+			*head = new;
+			printf("ALLOCATED %s %d\n", name, 0);
+			return new;
+		}
+		else {
+			printf("FAIL REQUEST %s %d\n", name, size);
+			return NULL;
+		}
+	}
+	else {
+		int worstSize = size - 1;
+		int worstLocation = -1;
+		int curr = 0;
+		while (currNode != NULL) {
+			if (currNode->start - curr > worstSize && currNode->start - curr >= size) {
+				worstSize = currNode->start - curr;
+				worstLocation = curr;
+			}
+			curr = currNode->start + currNode->size;
+			currNode = currNode->next;
+		}
+		if (totalMem - curr > worstSize && totalMem - curr >= size) {
+			worstSize = totalMem - curr;
+			worstLocation = curr;
+		}
+		if (worstLocation != -1) {
+			mem *new = (mem*) malloc(sizeof(mem));
+			new->start = worstLocation;
+			new->size = size;
+			strncpy(new->name, name, 16);
+			mem *start = *head;
+			if (start->start > worstLocation) {
+				new->next = *head;
+				*head = new;
+			}
+			else {
+				currNode = *head;
+				while (currNode != NULL && currNode->start + currNode->size < new->start) {
+					currNode = currNode->next;
+				}
+				new->next = currNode->next;
+				currNode->next = new;
+			}
+			printf("ALLOCATED %s %d\n", name, new->start);
+			return new;
+		}
+		else {
+			printf("FAIL REQUEST %s %d\n", name, size);
+			return NULL;
+		}
+	}
+}
+
+void release(mem *memory, char name[16], mem* next) {
 	mem *last;
 	while (memory != NULL && strcmp(memory->name, name) != 0) {
 		last = memory;
@@ -19,6 +252,10 @@ void release(mem *memory, char name[16]) {
 	if (memory != NULL) {
 		printf("FREE %s %d %d\n", name, memory->size, memory->start);
 		last->next = memory->next;
+		if (memory == next) {
+			next = NULL;
+		}
+		free(memory);
 	}
 	else {
 		printf("FAIL RELEASE %s\n", name);
@@ -37,8 +274,8 @@ void listAvailable(mem *memory, int totalMem) {
 		curr = memory->start + memory->size;
 		memory = memory->next;
 	}
-	if (curr <= end) {
-		printf("(%d, %d) ", end - curr + 1, curr);
+	if (curr < end) {
+		printf("(%d, %d) ", end - curr, curr);
 		full = 0;
 	}
 	if (full) {
@@ -83,21 +320,29 @@ int main(int argc, char** argv) {
 	mem *memory = NULL;
 
 	FILE *fp = fopen(argv[3], "r");
+	int type = -1;
 	if (strcmp(argv[1], "BESTFIT") == 0) {
 		printf("Best fit\n");
+		type = 0;
 	}
 	else if (strcmp(argv[1], "FIRSTFIT") == 0) {
 		printf("First fit\n");
+		type = 1;
 	}
 	else if (strcmp(argv[1], "NEXTFIT") == 0) {
 		printf("Next fit\n");
+		type = 2;
 	}
 	else if (strcmp(argv[1], "WORSTFIT") == 0) {
 		printf("Worst fit\n");
+		type = 3;
 	}
 	else {
 		printf("Invalid mode\n");
 	}
+
+	mem *next = NULL;
+
 	char line[64];
 	while (fgets(line, 64, fp) != NULL) {
 		if (line[0] == '#') {
@@ -109,13 +354,25 @@ int main(int argc, char** argv) {
 			char name[16];
 			int size;
 			sscanf(line, "%s %s %d\n", command, name, &size);
-			printf("REQUEST %s %d\n", name, size);
+			//printf("REQUEST %s %d\n", name, size);
+			if (type == 0) {
+				next = bestFit(&memory, name, size, totalMem);
+			}
+			else if (type == 1) {
+				next = firstFit(&memory, name, size, totalMem);
+			}
+			else if (type == 2) {
+				next = nextFit(&memory, name, size, totalMem, next);
+			}
+			else {
+				next = worstFit(&memory, name, size, totalMem);
+			}
 		}
 		else if (strcmp(command, "RELEASE") == 0) {
 			char name[16];
 			sscanf(line, "%s %s\n", command, name);
 			//printf("RELEASE %s\n", name);
-			release(memory, name);
+			release(memory, name, next);
 		}
 		else if (strcmp(command, "LIST") == 0) {
 			char command2[16];
